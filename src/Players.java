70eaf263402df754fbb9760696d7ebc8c5eb8b6b
Chase <Bainscr@jacks.sfasu.edu>
2022-10-06 15:44:57 -0500

    I think this will commit everything now

--- src/Players.java
+++ src/Players.java
@@ -1,40 +1,38 @@
-import cs331Helper.cs331TicTacToePlayer;
 import cs331Helper.cs331TicTacToeBoard;
+import cs331Helper.cs331TicTacToePlayer;
+import cs331Helper.cs331TicTacToeSquare;
+import cs331Helper.cs331TicTacToeController;
+
 
 public class Players implements cs331TicTacToePlayer{
 
-    
-    
-    private static int sumTrack = 1;
     private String playerIcon;
-    cs331TicTacToeBoard myBoard = new cs331TicTacToeBoard();
+    cs331TicTacToeBoard playerBoard;
+    cs331TicTacToeController playerController;
 
-    public Players(){
-        if(sumTrack%2 == 0){
-            playerIcon = "O";
-        }
-        else{
-            playerIcon = "X";
-        }
-        sumTrack = sumTrack + 1;
-        System.out.println("My icon is: " + playerIcon);
-        //myBoard.squareAt(0, 0);
+    public Players(){ //Default constructor
 
     }
 
+    public Players(cs331TicTacToeBoard b, cs331TicTacToeController c, String playerIcon){   
+        this.playerIcon = playerIcon;
+        System.out.println("My player icon is: " + playerIcon);
+        playerBoard = b;
+        playerController = c;
+    }
+
+    public String getPlayerIcon(){
+        return playerIcon;
+    }
+
     @Override
-    public void selectSquare(int row, int col) {
-        if(sumTrack%2 == 1){
-            myBoard.squareAt(row, col).markSquare("X");
-            
-        }
-        else{
-            myBoard.squareAt(row, col).markSquare("O");
-        }
-        sumTrack = sumTrack + 1;
+    public void selectSquare(int row, int col) {  
+        System.out.println("Player: selected square " + row + " " + col + " with icon: " + playerIcon);
+        playerBoard.squareAt(row, col).markSquare(playerIcon);
+        playerController.setControllerMessage("Player " + playerIcon + " has finished their turn");
+        playerController.finishedTurn();
         
-        
-        
+
     }
     
 }
--- src/myTicTacToe.java
+++ src/myTicTacToe.java
@@ -1,42 +1,63 @@
-import cs331Helper.cs331TicTacToeGame;
+//import cs331Helper.cs331TicTacToeGame;
 import cs331Helper.*;
-
-
+import javafx.scene.paint.Color;
+ 
 public class myTicTacToe implements cs331TicTacToeGame{
 
+    cs331TicTacToeController c1 = new cs331TicTacToeController();
+    public cs331TicTacToeBoard b1 = new cs331TicTacToeBoard();
+    //private int turnCounter = 1;
+    Players p1 = new Players(b1,c1,"X");
+    Players p2 = new Players(b1,c1,"O");
+    Color c = Color.YELLOWGREEN;
+    Color taken = Color.KHAKI;
+    
+
     public myTicTacToe(){
-        //Empty no args constructor
-        cs331TicTacToeBoard b1 = new cs331TicTacToeBoard();
-        cs331TicTacToeController c1 = new cs331TicTacToeController();
-        c1.setControllerMessage("Select a square to start playing");
-        Players p1 = new Players();
-        Players p2 = new Players();
+        
         c1.addPlayer(p1);
         c1.addPlayer(p2);
+        c1.setControllerMessage("Select a square to start playing!");
+
     }
 
+    
+
+    
+
     @Override
-    public void invalidSquareChosen(int arg0, int arg1) {
-        
-        
+    public void invalidSquareChosen(int row, int col) {
+        System.out.println("Invalid square chosen");
+        c1.setControllerMessage("Square has already been taken. Try again");
+        b1.squareAt(row, col).flashColor(taken);
     }
 
     @Override
     public void noWinner() {
-        // TODO Auto-generated method stub
+        System.out.println("Its a Tie!");
+        c1.setControllerMessage("Uh-Oh. Its a Tie");
+
         
     }
 
     @Override
     public void playerWins() {
-        // TODO Auto-generated method stub
-        
+        System.out.println("We have a winner!");
+        if(c1.getWinningPlayer()==p1){
+            c1.setControllerMessage("Player X has won!");
+        }
+        else if(c1.getWinningPlayer() == p2){
+            c1.setControllerMessage("Player O has won!");
+        }
+        b1.highlightWinningSquares(c);   
     }
 
     @Override
     public void restartGame() {
-        // TODO Auto-generated method stub
-        
+        b1.clearSymbols();
+        b1.clearHighlights();
+        c1.playAgain();
+        c1.setControllerMessage("Select a square to start playing!");
     }
 
 }
